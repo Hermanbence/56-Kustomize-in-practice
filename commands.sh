d921ea548b233faf2c058c5207c7c0f691bc3378
Ben <107430840+Hermanbence@users.noreply.github.com>
2024-06-24 15:37:15 +0200

    finished exercise

--- commands.sh
+++ commands.sh
@@ -1,9 +1,17 @@
 kubectl config view --minify
 kubectl create namespace home
-kubectl create namespace home
 kubectl config set-context --current --namespace=home
 kubectl config view --minify
 kubectl kustomize .
+- render, localy
 kubectl apply --dry-run=server -k .
+- server side 
+    - like no naming problem etc.
+-k kustomization.yaml file
 kubectl apply -k .
 kubectl get pods
+kubectl get replicaset -n kustom
+kubectl rollout undo deploy/example -n kustom
+kubectl get cm -n kustom
+- cm --> configmap
+kubectl delete cm -l app=example -n kustom
